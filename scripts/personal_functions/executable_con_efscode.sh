8cc3ea50985f45a1737c8e72bd34a9c38bc946e7
EdimarDeSa <edimar.sa@efscode.com.br>
2025-06-13 11:13:00 -0300

    Ajustes de configurações

--- scripts/personal_functions/executable_con_efscode.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-echo "Conectando no servidor de produção!"
-ssh -i "$HOME/.ssh/hostinger" efscode@efscode
--- scripts/personal_functions/executable_script_con_dev_server.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-echo "Conectando no servidor de desenvolvimento!"
-ssh -i "$HOME/.ssh/hostinger" edimar@edimar-default-string
